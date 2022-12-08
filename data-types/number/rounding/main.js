575d45a586d5a1e19bc9ed7288fe4d9bb34aa8cc
Damian Padilla <damipad4@gmail.com>
2022-12-07 23:44:07 -0600

    init number rounding

---
+++ data-types/number/rounding/main.js
@@ -0,0 +1,33 @@
+/*
+***************
+Number Rounding
+***************
+
+One of the Most used operations when working with numbers is rounding
+
+There are several built-in functions for rounding:
+
+Math.floor
+Rounds down:
+
+Math.ceil
+Rounds Up
+
+Math.round
+Rounds to the nearest integer: 3.1 becomes 3, 3.6 becomes 4
+*/
+
+// Math.floor
+let floor = 1.23456
+
+console.log(Math.floor(floor));
+
+// Math.ceil
+let ceil = 22.00001
+
+console.log(Math.ceil(ceil));
+
+// Math.round
+let round = 2.5
+
+console.log(Math.round(round));
