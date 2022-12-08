4b0ac733ff61ed0dac0667369f68e6923f9edcdb
Damian Padilla <damipad4@gmail.com>
2022-12-07 23:31:14 -0600

    Added str length -str fundamentals

--- data-types/strings/string-fundamentals/main.js
+++ data-types/strings/string-fundamentals/main.js
@@ -47,3 +47,27 @@ Special characters can be encoded using escape sequences
 let newLine = '\n New Line'
 let quotes = 'This \"encodes\" the different kinds of quotes. Isn\'t that awesome? '
 console.log(quotes);
+
+
+/*
+*************
+String Length
+*************
+
+The length property has the string length
+*/
+
+let stringLength = 'Helllooooooo'
+console.log(`The string length is ${stringLength.length}`);
+
+/*
+IMPORTANT NOTE: 
+People with a background in some other languages sometimes mistype by calling str.length() instead of just str.length. That doesn't work.
+
+Please note that str.length is a numeric property, NOT A FUNCTION. There is no need to add parenthesis after it. 
+
+tldr; 
+
+USE .length
+NOT .length()
+*/
